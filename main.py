675e638f2917667e2ad859c219029e9d4069bede
Andre Fidelis <andre.fidelis@linx.com.br>
2025-05-12 21:58:08 -0300

    gerando soluções iniciais aleatorias

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('startup do projeto')
+import numpy as np
+
+
+def pso(dim = 3, num_particles = 30, max_iter=100, w=0.5, c1=2.05, c2=2.05):
+    #inicializa as particulas e as velocidades
+    particles = np.random.uniform(0, 1, (num_particles, dim))
+    velocities = np.zeros((num_particles, dim))
+    
+    best_positions = np.copy(particles)
+
+pso()
+
+
